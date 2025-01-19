27057b0c097d785032b84b66a6264fbbc31c063e
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2025-01-19 15:15:13 +0000

    following button is now updating depending on the follow state

--- client/src/parts/UserCard.tsx
+++ client/src/parts/UserCard.tsx
@@ -1,29 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function UserCard({ user, me, session, token }) {
 
   let button = <></>;
+  if(!user) return <></>;
+
+  const [followers, setFollowers] = useState(user.followers.length);
+  const [followLine, setFollowline] = useState(null);
+
+  const follow = async () => {
+    const url = session.apiUrl + "/follow?username=" + user.username;
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Authorization": "Bearer " + token
+      }
+    })
+
+    if(response.ok) {
+      const userInfos = await response.json();
+      setFollowers(userInfos.userB.followers.length);
+
+      if(userInfos.userB.followers.includes(userInfos.user.username)) {
+        setFollowline("Ne plus suivre");
+      } else {
+        setFollowline("Commencer à suivre");
+      }
+    }
+  }
 
   if(me) {
     if(me.username == user.username) {
       // button = <button className="inverted">Modifier mon profil</button>
     } else {
-      if(me.following.includes(user.username)) {
-        button = <button>Ne plus suivre</button>
+
+      if(followLine) {
+        button = <button onClick={follow}>{followLine}</button>
       } else {
-        button = <button>Commencer à suivre</button>
+        if(me.following.includes(user.username)) {
+          button = <button onClick={follow}>Ne plus suivre</button>
+        } else {
+          button = <button onClick={follow}>Commencer à suivre</button>
+        }
       }
     }
   }
 
-  return user && <div className="profile bubble"
+  return <div className="profile bubble"
     style={{ border: "1px solid black "}}>
     <div className="cols" style={{ gap: "20px" }} >
       <img className="avatar" src={user.avatar && user.avatar.length ? user.avatar :
       `${session.apiUrl.replaceAll('/api', '')}/assets/user.png` } />
       <div className="badges">{
         user.badges.map((badge: string) => {
-          return <img 
+          return <img
             src={`${window.location.origin}/static/badges/${badge}.png`} />
         })
       }</div>
@@ -31,9 +61,7 @@ export default function UserCard({ user, me, session, token }) {
     <h1>{user.fullname}</h1>
     <span><span>@{user.username}</span> - <span>{user.pronouns}</span></span>
     <span>
-      <span>{user.followers.length}</span> abonnés - <span>{
-      user.following.length
-      }</span> abonnements</span>
+      <span>{followers}</span> abonnés - <span>{user.following.length}</span> abonnements</span>
     {button}
     <p>{user.description}</p>
   </div>
